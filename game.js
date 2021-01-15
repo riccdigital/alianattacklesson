f7081d097f13f37453f7578de0dcb1a779f5128a
riccdigital <eryc.mabonga@gmail.com>
2021-01-15 04:13:04 +0300

    JS Variables Set Up

--- game.js
+++ game.js
@@ -1,10 +1,43 @@
 // == GAME VARIABLES == //
-/* Alien
-        Position */
+// Alien Position //
 let alienX = 80
 let alienY = 20
 
-/* Player
-        Position */
+// Player Position //
 let guessX = 0
 let guessY = 0
+
+// Game State //
+const shotsRemaining = 10
+let shotsMade = 0
+let gameState = ''
+let gameWon = false
+
+// Game Objects//
+const cannon = document.getElementById('cannon')
+const alien = document.getElementById('alien')
+const missile = document.getElementById('missile')
+
+// Controls //
+const output = document.getElementById('output')
+const inputY = document.getElementById('inputY')
+const inputX = document.getElementById('inputX')
+
+const button = document.querySelector('button')
+button.style.cursor = 'pointer'
+button.addEventListener('click', clickHandler, false)
+
+function clickHandler() {
+  validateInput()
+}
+
+function validateInput() {
+  guessX = parseInt(inputX.value)
+  guessY = parseInt(inputY.value)
+
+  if (isNaN(guessX) || isNaN(guessY)) {
+    output.innerHTML = 'Please enter a number!'
+  } else {
+    playGame()
+  }
+}
